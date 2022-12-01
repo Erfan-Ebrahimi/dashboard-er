d9e4cc819f062d69d04d1157a0b657c8979fb148
Ebrahimi.Erfan <ebrahimi.erfan89@gmail.com>
2022-12-01 04:26:47 +0330

    Topbar & Siadebar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+// import { Routes , Route } from 'react-router-dom';
+//---- COMPONENTS ----------//
+import Topbar from "./components/topbar/Topbar";
+import Sidebar from './components/sidebar/Sidebar';
+
+//------- PAGES  ---------//
+import Home from './pages/home/Home';
+
+//------- STYLE  ---------//
 import "./App.css";
 
 const App = () => {
   return (
-    <div className='container'>
-      <p>App</p>
+    <div >
+      <Topbar/>
+      <div className='container'>
+      <Sidebar/>
+      <Home/>
+      </div>
     </div>
   )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+
+
+// ---------- STYLE -----------//
+import "./home.scss"
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home'>Home</div>
   )
 }
 
-export default Home
+export default Home;
